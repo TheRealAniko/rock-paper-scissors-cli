0871e3d32b9301d960f452ee7142274a9b801d94
“Aniko” <aniko.juhasz@yahoo.com>
2025-01-10 16:14:31 +0100

    Initial game logic plan with comments

---
+++ ro-pa-si.js
@@ -0,0 +1,17 @@
+// example.js
+const args = process.argv.slice(2);
+console.log(args);
+
+// Check if players input is a single value; if not log message
+
+// Check if players input is "rock", "paper" or "scissor"; if not log message
+
+// Spellcheck? Handle upper and lower case inputs
+
+// System randomly selects a value from options: "rock", "paper" or "scissor"
+
+// Compare players input with systems value
+
+// Define the rules for winning/loosing
+
+// Output the result
