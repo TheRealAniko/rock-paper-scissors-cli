67605bf1dfa41c1258a0d23aef2f466ca28f22d5
“Aniko” <aniko.juhasz@yahoo.com>
2025-01-11 11:06:36 +0100

    Added players input

--- ro-pa-si.js
+++ ro-pa-si.js
@@ -2,10 +2,18 @@
 const args = process.argv.slice(2);
 console.log(args);
 
+const options = ["rock", "paper", "scissor"];
+
 // Check if players input is a single value; if not log message
-
+if (args.length !== 1) {
+    console.error("Pleaser enter just one option!");
+    return;
+}
 // Check if players input is "rock", "paper" or "scissor"; if not log message
-
+if (!args.includes("rock", "paper", "scissor")) {
+    console.error("Pleaser enter rock, paper or scissor!");
+    return;
+}
 // Spellcheck? Handle upper and lower case inputs
 
 // System randomly selects a value from options: "rock", "paper" or "scissor"
