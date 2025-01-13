c3daad04ab87242dff102e43f788e626b1acd593
“Aniko” <aniko.juhasz@yahoo.com>
2025-01-13 18:33:54 +0100

    Compare players input with systems value & Define the rules for winning/loosing

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,7 +1,7 @@
 // example.js
 const args = process.argv.slice(2);
-
 const options = ["rock", "paper", "scissor"];
+const playerMove = args[0].trim().toLowerCase();
 
 // Check if players input is a single value; if not log message
 if (args.length !== 1) {
@@ -9,21 +9,29 @@ if (args.length !== 1) {
     return;
 }
 // Check if players input is "rock", "paper" or "scissor"; if not log message
-if (!args.includes("rock", "paper", "scissor")) {
+
+if (!options.includes(playerMove)) {
     console.error("Pleaser enter rock, paper or scissor!");
     return;
 }
-// Spellcheck? Handle upper and lower case inputs
 
 // System randomly selects a value from options: "rock", "paper" or "scissor"
 const randomOption = (options) =>
     options[Math.floor(Math.random() * options.length)];
+const computerMove = randomOption(options);
 
-// console.log(randomOptions(options));
-
-// Compare players input with systems value
-
-// Define the rules for winning/loosing
+// Compare players input with systems value & Define the rules for winning/loosing
+if (playerMove === computerMove) {
+    console.log("It is a tie! Great minds think alike.");
+} else if (playerMove === "rock" && computerMove === "scissors") {
+    console.log("You crushed it! The computer had no chance.");
+} else if (playerMove === "paper" && computerMove === "rock") {
+    console.log("You crushed it! The computer had no chance.");
+} else if (playerMove === "scissor" && computerMove === "paper") {
+    console.log("You crushed it! The computer had no chance.");
+} else {
+    console.log("Ouch! The computer wins this one.");
+}
 
 // Output the result
-console.log(`Your input: ${args} / Computers input: ${randomOption(options)}`);
+console.log(`Your input: ${args} / Computers input: ${computerMove}`);
